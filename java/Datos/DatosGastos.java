37e01838ff45e942abeec6d8682c8ba9e4c70afb
Antix199 <142464955+Antix199@users.noreply.github.com>
2023-12-09 19:01:47 -0300

    Add files via upload

---
+++ java/Datos/DatosGastos.java
@@ -0,0 +1,94 @@
+package Datos;
+
+import Modelo.Gasto;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DatosGastos {
+
+    public static void guardarGastoEnCSV(String nombre, double cantidad, String categoria, String correoUsuario) {
+        String rutaArchivo = obtenerRutaArchivo(correoUsuario);
+
+        try {
+            FileWriter escritor = new FileWriter(rutaArchivo, true);
+
+            if (!archivoExiste(rutaArchivo)) {
+                // Write the header only if the file doesn't exist
+                escribirEncabezado(escritor);
+            }
+
+            escribirGasto(escritor, nombre, cantidad, categoria);
+
+            escritor.close();
+            System.out.println("Gasto registrado con éxito.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private static String obtenerRutaArchivo(String correoUsuario) {
+        String rutaDirectorio = System.getProperty("user.dir") + File.separator + "gastos_por_usuarios";
+        File directorio = new File(rutaDirectorio);
+        directorio.mkdirs();
+
+        return rutaDirectorio + File.separator + correoUsuario + "_gastos.csv";
+    }
+
+    private static boolean archivoExiste(String rutaArchivo) {
+        File archivo = new File(rutaArchivo);
+        return archivo.exists() && archivo.length() > 0; // Que no este vacío
+    }
+
+    private static void escribirEncabezado(FileWriter escritor) throws IOException {
+        escritor.write("Nombre del Producto,Precio,Categoria\n");
+    }
+
+
+    private static void escribirGasto(FileWriter escritor, String nombre, double cantidad, String categoria) throws IOException {
+        escritor.write(nombre + "," + cantidad + "," + categoria + "\n");
+    }
+
+    public static List<Gasto> cargarGastos(String correoUsuario) {
+        List<Gasto> gastos = new ArrayList<>();
+        String rutaArchivo = obtenerRutaArchivo(correoUsuario);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(rutaArchivo))) {
+            procesarArchivo(reader, gastos, correoUsuario);
+        } catch (IOException e) {
+            System.out.println("No se encontro el archivo de gastos del usuario");
+        }
+
+        return gastos;
+    }
+
+    private static void procesarArchivo(BufferedReader reader, List<Gasto> gastos, String correoUsuario) throws IOException {
+        String linea;
+        boolean primeraLinea = true;
+
+        while ((linea = reader.readLine()) != null) {
+            if (primeraLinea) {
+                primeraLinea = false;
+                continue;
+            }
+
+            procesarLinea(linea, gastos, correoUsuario);
+        }
+    }
+
+    private static void procesarLinea(String linea, List<Gasto> gastos, String correoUsuario) {
+        String[] partes = linea.split(",");
+        if (partes.length == 3) {
+            Gasto gasto = new Gasto(partes[0], Double.parseDouble(partes[1]), partes[2], correoUsuario);
+            gastos.add(gasto);
+        } else {
+            System.out.println("Error en el formato de línea del archivo CSV.");
+        }
+    }
+
+
+}
---
+++ java/Guis/GuiInicioSesion.java
@@ -0,0 +1,57 @@
+package Guis;
+
+import Modelo.CuentaUsuario;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuiInicioSesion extends JFrame {
+    private JPanel inicioSesion;
+    private JTextField correoText;
+    private JPasswordField contrasenaText;
+    private JButton iniciarSesionButton;
+    private JButton crearCuentaButton;
+    private JButton finalizarProgramaButton;
+    private JLabel icono;
+
+    private String rutaImagen = "src/main/java/Guis/icono.png";
+
+    public GuiInicioSesion() {
+
+        super("Menu inicio de sesión");
+        setContentPane(inicioSesion);
+
+        ManejoGuis.mostrarIcono(rutaImagen,icono,100);
+        iniciarSesionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String correo = correoText.getText().trim().toLowerCase();
+                String contrasena= new String(contrasenaText.getPassword());
+
+                Modelo.Usuario usuario = CuentaUsuario.iniciarSesion(correo, contrasena);
+
+                if (usuario != null) {
+                    // Inicio de sesión exitoso
+                    ManejoGuis.abrirGuiPrincipal(usuario);
+                    dispose();
+                    JOptionPane.showMessageDialog(null, "Inicio de sesión exitoso. ¡Bienvenid@, " + usuario.getNombre() + "!");
+                } else {
+                    // Inicio de sesión fallido
+                    JOptionPane.showMessageDialog(null,"Inicio de sesión fallido. Verifica tus credenciales.");
+                }
+            }
+        });
+        crearCuentaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ManejoGuis.abrirGuiCrearCuenta();
+            }
+        });
+        finalizarProgramaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ManejoGuis.finalizarPrograma();
+            }
+        });
+    }
+}
