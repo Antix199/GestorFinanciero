7fc65623166798ab7ad724bed9ca290a97e24fb0
Antix199 <142464955+Antix199@users.noreply.github.com>
2023-12-10 21:15:11 -0300

    Update CalculadoraPorcentajeGastosTest.java

--- GestorFinanciero_/src/test/java/Test/CalculadoraPorcentajeGastosTest.java
+++ GestorFinanciero_/src/test/java/Test/CalculadoraPorcentajeGastosTest.java
@@ -11,17 +11,17 @@ public class CalculadoraPorcentajeGastosTest {
 
     @Test
     public void testCalcularTotalGastadoPorCategoria() {
-        // Arrange (Configuración)
+       
+    
         List<Gasto> listaDeGastos = new ArrayList<>();
         listaDeGastos.add(new Gasto("Gasto1", 50.0, "Comida", "correo1"));
         listaDeGastos.add(new Gasto("Gasto2", 30.0, "Transporte", "correo1"));
         listaDeGastos.add(new Gasto("Gasto3", 20.0, "Comida", "correo1"));
         listaDeGastos.add(new Gasto("Gasto4", 40.0, "Entretenimiento", "correo1"));
 
-        // Act (Acción)
+     
         double totalCalculado = CalculadoraPorcentajeGastos.calcularTotalGastadoPorCategoria(listaDeGastos, "Comida");
 
-        // Assert (Afirmación)
         assertEquals(70.0, totalCalculado, 0.001);
     }
 }
